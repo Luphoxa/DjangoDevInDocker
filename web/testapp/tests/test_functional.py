a869d9f873d1c5086b2f5162d7a9116e7dd7aec0
Irene Herrero <herreroi@in.tum.de>
2018-03-14 17:49:06 +0100

    a functioning version

--- web/testapp/tests/test_functional.py
+++ web/testapp/tests/test_functional.py
@@ -9,20 +9,20 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 class SeleniumTest(TestCase):
     def setUp(self):
         self.chrome = webdriver.Remote(
-            command_executor='http://selenium_hub:4444/wd/hub',
+            command_executor='http://selenium-hub:4444/wd/hub',
             desired_capabilities=DesiredCapabilities.CHROME
         )
         self.chrome.implicitly_wait(10)
         self.firefox = webdriver.Remote(
-            command_executor='http://selenium_hub:4444/wd/hub',
+            command_executor='http://selenium-hub:4444/wd/hub',
             desired_capabilities=DesiredCapabilities.FIREFOX
         )
         self.firefox.implicitly_wait(10)
 
     def test_visit_site_with_chrome(self):
-        self.chrome.get('http://django:8000')
+        self.chrome.get('http://web:8000')
         self.assertIn(self.chrome.title, 'Django: the Web framework for perfectionists with deadlines.')
 
     def test_visit_site_with_firefox(self):
-        self.firefox.get('http://django:8000')
+        self.firefox.get('http://web:8000')
         self.assertIn(self.firefox.title, 'Django: the Web framework for perfectionists with deadlines.')
