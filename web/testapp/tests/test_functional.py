1bfc2994dc2adccaefd6531e95befdcf395dedbc
Irene Herrero <herreroi@in.tum.de>
2018-03-14 17:30:50 +0100

    simple test works

--- web/testapp/tests/test_functional.py
+++ web/testapp/tests/test_functional.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 
-from django.test import TestCase
-
-
+#
 from django.test import TestCase
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
